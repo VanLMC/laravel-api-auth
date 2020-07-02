86a2cdca0d2db4e387f2f72dc8d6121beb44d0e5
Vanderlei <vanderleichagas42@gmail.com>
2020-07-02 00:24:52 -0300

    login funcionando

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 */
 
 Route::get('users', 'Api\UserController@index');
+
+Route::post('login', 'Api\AuthController@login');
